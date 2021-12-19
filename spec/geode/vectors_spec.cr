6c1e800c3240396f3e3875d726f7aa424ccd2abc
Michael Miller <icy.arctic.fox@gmail.com>
2021-12-18 21:11:13 -0700

    Add min and max vector methosd

---
+++ spec/geode/vectors_spec.cr
@@ -0,0 +1,34 @@
+require "../spec_helper"
+
+Spectator.describe Geode do
+  let(v1) { Geode::Vector[3, 5, 7] }
+  let(v2) { Geode::Vector[7, 5, 3] }
+
+  describe "#min" do
+    context "with two vectors" do
+      it "returns the lesser components" do
+        expect(Geode.min(v1, v2)).to eq(Geode::Vector[3, 5, 3])
+      end
+    end
+
+    context "with a vector and value" do
+      it "returns the lesser component or value" do
+        expect(Geode.min(v1, 5)).to eq(Geode::Vector[3, 5, 5])
+      end
+    end
+  end
+
+  describe "#max" do
+    context "with two vectors" do
+      it "returns the greater components" do
+        expect(Geode.max(v1, v2)).to eq(Geode::Vector[7, 5, 7])
+      end
+    end
+
+    context "with a vector and value" do
+      it "returns the greater component or value" do
+        expect(Geode.max(v1, 5)).to eq(Geode::Vector[5, 5, 7])
+      end
+    end
+  end
+end
--- src/geode/vectors.cr
+++ src/geode/vectors.cr
@@ -1,4 +1,39 @@
 require "./vectors/**"
 
 module Geode
+  # Returns a new vector with the minimum component from each vector
+  #
+  # ```
+  # Geode.min(Vector[1, 2, 3], Vector[3, 2, 1]) # => (1, 2, 1)
+  # ```
+  def self.min(a : CommonVector(T, N), b : CommonVector(T, N)) : CommonVector forall T, N
+    a.class.new { |i| Math.min(a.unsafe_fetch(i), b.unsafe_fetch(i)) }
+  end
+
+  # Returns a new vector with the lesser value of the component or *value*.
+  #
+  # ```
+  # Geode.min(Vector[1, 2, 3], 2) # => (1, 2, 2)
+  # ```
+  def self.min(vector : CommonVector(T, N), value : T) : CommonVector forall T, N
+    vector.class.new { |i| Math.min(vector.unsafe_fetch(i), value) }
+  end
+
+  # Returns a new vector with the maximum component from each vector.
+  #
+  # ```
+  # Geode.max(Vector[1, 2, 3], Vector[3, 2, 1]) # => (3, 2, 3)
+  # ```
+  def self.max(a : CommonVector(T, N), b : CommonVector(T, N)) : CommonVector forall T, N
+    a.class.new { |i| Math.max(a.unsafe_fetch(i), b.unsafe_fetch(i)) }
+  end
+
+  # Returns a new vector with the greater value of the component or *value*.
+  #
+  # ```
+  # Geode.max(Vector[1, 2, 3], 2) # => (2, 2, 3)
+  # ```
+  def self.max(vector : CommonVector(T, N), value : T) : CommonVector forall T, N
+    vector.class.new { |i| Math.max(vector.unsafe_fetch(i), value) }
+  end
 end
